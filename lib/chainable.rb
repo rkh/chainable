7ac6c165b3029ffcc8cfdcf9c28bc6b2a072860b
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2009-03-19 23:40:57 +0100

    some cleanup

--- lib/chainable.rb
+++ lib/chainable.rb
@@ -1,29 +1,22 @@
-warn  "Warning: you just sold your soul to the devil."
-
-begin
-  require "ruby2ruby"
-rescue LoadError
-end
+require "ruby2ruby"
+require "thread"
 
 module Chainable
+
+  def self.skip_chain
+    return if @auto_chain
+    @auto_chain = true
+    yield
+    @auto_chain = false
+  end
 
   def chain_method(name, optimize = true, &block)
     name = name.to_s
     if instance_methods(false).include? name
-      if optimize and defined? Ruby2Ruby
+      begin
         code = Ruby2Ruby.translate self, name
         include Module.new { eval code }
-      else
-        # HACK. This part is pretty much a mean hack. Any super in the original
-        # method has the potential to screw this up big time.
-        # But Ruby2Ruby is not able to translate methods written in C (in MRI).
-        # Maybe fall back to alias_method_chain?
-        @__chained__ ||= []
-        if @__chained__.include? name
-          error = "you cannot chain a method twice without Ruby2Ruby."
-          error << " do not set +optimize+ to false." unless optimize
-          raise ArgumentError, error
-        end
+      rescue NameError
         m = instance_method name
         include Module.new { define_method(name) { |*a, &b| m.bind(self).call(*a, &b) } }
       end
@@ -35,19 +28,12 @@ module Chainable
   def auto_chain &block
     raise ArgumentError, "no block given" unless block_given?
     result = nil
-    class_eval do
-      class << self
-        chain_method :method_added do |name|
-          return if @__chaining__
-          @__chaining__ = true
-          chain_method name
-          @__chaining__ = false
-        end
+    class << self
+      chain_method :method_added do |name|
+        Chainable.skip_chain { chain_method name }
       end
       result = block.call
-      class << self
-        remove_method :method_added
-      end
+      remove_method :method_added
     end
     result
   end
@@ -56,5 +42,4 @@ end
 
 Module.class_eval do
   include Chainable
-  private "auto_chain", "chain_method"
 end
