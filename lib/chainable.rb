d9c90611b6ae7d9df7fd4100b0e05047996e165f
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2009-03-20 17:37:35 +0100

    added comments

--- lib/chainable.rb
+++ lib/chainable.rb
@@ -9,6 +9,11 @@ module Chainable
     @auto_chain = false
   end
 
+  # This will "chain" a method (read: push it to a module and include it).
+  # If a block is given, it will do a define_method(name, &block).
+  # Maybe that is not what you want, as methods defined by def tend to be
+  # faster. If that is the case, simply don't pass the block and call def
+  # after chain_method instead.
   def chain_method(name, &block)
     name = name.to_s
     if instance_methods(false).include? name
@@ -24,7 +29,11 @@ module Chainable
     define_method(name, &block)
   end
 
-  def auto_chain &block
+  # If you define a method inside a block passed to auto_chain, chain_method
+  # will be called on that method right after it has been defined. This will
+  # only affect methods defined for the class (or module) auto_chain has been
+  # send to. See README.rdoc or spec/chainable/auto_chain_spec.rb for examples.
+  def auto_chain
     class << self
       chain_method :method_added do |name|
         Chainable.skip_chain { chain_method name }
