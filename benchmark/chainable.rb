f29f8e22352f7a1b429bb434501b22946079e102
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2009-03-22 15:27:22 +0100

    benchmark restructured

--- benchmark/chainable.rb
+++ benchmark/chainable.rb
@@ -2,36 +2,35 @@ require "benchmark"
 require "lib/chainable"
 require "active_support"
 
-class BenchmarkChainable
-  def name
-    "chainable"
-  end
-  def bm1
-    100
-  end
-  def bm2
-    100
-  end
-  1000.times do
-    chain_method(:bm1) { super * 2 }
-    chain_method :bm2
-    def bm2
-      super * 2
+class BenchmarkChain
+  CHAIN_LENGTH = 1000
+  CALL_TIMES = 100
+  class << self
+    def bm1(x)
+      obj = new
+      x.report("#{@name} (define_method)") { CALL_TIMES.times { obj.bm1 } }
     end
+    def bm2(x)
+      obj = new
+      x.report("#{@name} (def & eval)") { CALL_TIMES.times { obj.bm2 } }
+    end
+  end
+  define_method(:bm1) { 100 }
+  def bm2; 100; end
+end
+
+class BenchmarkChainable < BenchmarkChain
+  @name = "chainable"
+  CHAIN_LENGTH.times do
+    chain_method(:bm1) { super * 2 }
+    chain_method(:bm2)
+    def bm2; super * 2; end
   end
 end
 
-class BenchmarkAliasMethodChain
-  def name
-    "alias_method_chain"
-  end
-  def bm1
-    100
-  end
-  def bm2
-    100
-  end
-  1000.times do |i|
+class BenchmarkAliasMethodChain < BenchmarkChain
+  @name = "alias_method_chain"
+  CHAIN_LENGTH.times do |i|
     define_method("bm1_with_#{i}") { send("bm1_without_#{i}") * 2 }
     alias_method_chain :bm1, i.to_s
     eval "def bm2_with_#{i}; bm2_without_#{i} * 2; end"
@@ -41,11 +40,11 @@ end
 
 def bench(x, klass)
   obj = klass.new
-  x.report("#{obj.name} (define_method)") { 100.times { obj.bm1 } }
-  x.report("#{obj.name} (def & eval)") { 100.times { obj.bm2 } }
 end
 
 Benchmark.bmbm do |x|
-  bench(x, BenchmarkChainable)
-  bench(x, BenchmarkAliasMethodChain)
+  BenchmarkChainable.bm1(x)
+  BenchmarkAliasMethodChain.bm1(x)
+  BenchmarkChainable.bm2(x)
+  BenchmarkAliasMethodChain.bm2(x)
 end
